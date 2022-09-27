601ddff1ace9b0f8a6140e8a2d3e799db956e3cb
Ken Salanio <kssalanio@gmail.com>
2022-09-27 15:23:47 +0800

    updated django.urls import

--- djangomapserver/models.py
+++ djangomapserver/models.py
@@ -6,7 +6,7 @@ Django models that align with MapServer's mapscript API
 
 import os
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
